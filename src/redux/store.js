914f64171bdf7df3ac6dece67ed4b5b62885ede5
wangjn_bj@si-tech.com.cn <12345678>
2022-07-02 16:21:28 +0800

    测试提交

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import {createStore,combineReducers} from 'redux'
 
 import count from './reducers/count'
 import personList from './reducers/person'
-const Allreducer=combineReducers({
+const Allreducers=combineReducers({
     count,
     personList})
-export default createStore(Allreducer)
+export default createStore(Allreducers)
